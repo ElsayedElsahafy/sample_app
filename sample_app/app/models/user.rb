b1435669af3ad1ae9dfbb775671e1e25865fd4f4
elsayed <elsayed.fathy90@yahoo.com>
2014-06-21 09:40:29 +0200

    follow buttum

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
   has_many :microposts , dependent: :destroy
+#followed_user relation
+  has_many :relationships , foreign_key: "follower_id" , dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+#follower_user relation
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+ 
 
   #constans
    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -26,16 +35,29 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
-  def feed
-    Micropost.where("user_id = ? " , id)
-    
-  end
-
-  private
-
-    def create_remember_token
+  def create_remember_token
       self.remember_token = User.digest(User.new_remember_token)
     end
 
+
+  def feed
+    Micropost.where("user_id = ? " , id)
+  end
+
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+
+def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
+  end
+
 end
+
                 
--- sample_app/lib/tasks/sample_data.rake
+++ sample_app/lib/tasks/sample_data.rake
@@ -26,4 +26,13 @@ namespace :db do
       end
 
     end
+
+   def make_relationships
+  users = User.all
+  user  = users.first
+  followed_users = users[2..50]
+  followers      = users[3..40]
+  followed_users.each { |followed| user.follow!(followed) }
+  followers.each      { |follower| follower.follow!(user) }
+end
 end
